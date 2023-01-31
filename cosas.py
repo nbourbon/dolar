5209c3252d704ef7a0497dc096f54ee0460dacd2
nbourbon <nicolas_bourbon@yahoo.com.ar>
2023-01-31 19:03:08 -0300

    vamos a pullear al mail

--- cosas.py
+++ cosas.py
@@ -6,7 +6,8 @@ def carga():
     cotizaciones = {"blue_c":valor[0].text, "blue_v":valor[1].text, "ofi_c":valor[4].text, "ofi_v":valor[5].text, 
                     "mep_c":valor[6].text, "mep_v":valor[7].text, "ccl_c":valor[8].text, "ccl_v":valor[9].text, 
                     "cripto_c":valor[10].text, "cripto_v":valor[11].text}
-    database1 = pd.DataFrame.from_dict(cotizaciones, orient ='index')
-    database1.columns =[datetime.now()]
+    database2 = pd.DataFrame.from_dict(cotizaciones, orient ='index')
+    database2.columns =[datetime.now()]
     #return test in cosas
-    return(database1)
+    # Además agrego acá
+    return(database2)
